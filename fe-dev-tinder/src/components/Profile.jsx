0266dab8c1e512e6d55e796268831eff338baa8e
Rohan Gore <gorerohan15@gmail.com>
2025-03-12 07:47:10 +0530

    updated the redirect url

--- fe-dev-tinder/src/components/Profile.jsx
+++ fe-dev-tinder/src/components/Profile.jsx
@@ -30,9 +30,24 @@ const Profile = () => {
         redirectTarget: "_self",
       };
 
-      const cfResponse = await cashfree.checkout(checkoutOptions);
-
-      console.log({ cfResponse });
+      cashfree.checkout(checkoutOptions).then((result) => {
+        if (result.error) {
+          // This will be true when there is any error during the payment
+          console.log("There is some payment error, Check for Payment Status");
+          console.log(result.error);
+        }
+        if (result.redirect) {
+          // This will be true when the payment redirection page couldnt be opened in the same window
+          // This is an exceptional case only when the page is opened inside an inAppBrowser
+          // In this case the customer will be redirected to return url once payment is completed
+          console.log("Payment will be redirected");
+        }
+        if (result.paymentDetails) {
+          // This will be called whenever the payment is completed irrespective of transaction status
+          console.log("Payment has been completed, Check for Payment Status");
+          console.log(result.paymentDetails.paymentMessage);
+        }
+      });
     } catch (err) {
       console.error(err);
     }
